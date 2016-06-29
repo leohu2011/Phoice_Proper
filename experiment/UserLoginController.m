d82c8cdbfc38f6d687aaf661f3664a41fa155855
Qi Hu <qihu@Qi-Hus-MacBook-Pro.local>
2016-06-29 10:24:11 +0800

    interface ready

--- experiment/UserLoginController.m
+++ experiment/UserLoginController.m
@@ -19,6 +19,20 @@
     UISegmentedControl *segControl;
     UIButton *autoButton;
     UIButton *rememberButton;
+    
+    UIBarButtonItem *item_login;
+    UIBarButtonItem *item_logout;
+    UIBarButtonItem *item_register;
+    UIBarButtonItem *flexItem;
+    
+    UITextField *nameField;
+    UITextField *passwordField;
+    UITextField *passwordField2;
+    
+    
+    
+    
+    
 }
 
 -(void)viewWillAppear:(BOOL)animated{
@@ -49,12 +63,36 @@
     tap.numberOfTouchesRequired = 1;
     [self.view addGestureRecognizer:tap];
     
-    UIBarButtonItem *item1 = [[UIBarButtonItem alloc]initWithTitle:@"Log In" style:UIBarButtonItemStyleDone target:self action:@selector(loginCheck)];
-    item1.tintColor = [UIColor whiteColor];
-    UIBarButtonItem *item2 = [[UIBarButtonItem alloc]initWithTitle:@"Log Out" style:UIBarButtonItemStyleDone target:self action:@selector(logoutCheck)];
-    UIBarButtonItem *flexItem = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
     
-    [self setToolbarItems:@[flexItem,item1,flexItem, item2, flexItem] animated:YES];
+    CGFloat indent = 10.f;
+    
+    nameField = [[UITextField alloc]init];
+    nameField.frame = CGRectMake(indent, 250, width - indent*2, 50);
+    nameField.placeholder = @"User Name";
+    nameField.textAlignment = NSTextAlignmentCenter;
+    nameField.borderStyle = UITextBorderStyleRoundedRect;
+    [self.view addSubview:nameField];
+    
+    passwordField = [UITextField new];
+    passwordField.frame = CGRectMake(indent, 320, width-indent*2, 50);
+    passwordField.placeholder = @"Password";
+    passwordField.textAlignment = NSTextAlignmentCenter;
+    passwordField.borderStyle = UITextBorderStyleRoundedRect;
+    [self.view addSubview:passwordField];
+    
+    passwordField2 = [UITextField new];
+    passwordField2.frame = CGRectMake(indent, 390, width-indent*2, 50);
+    passwordField2.placeholder = @"Enter Password Again";
+    passwordField2.textAlignment = NSTextAlignmentCenter;
+    passwordField2.borderStyle = UITextBorderStyleRoundedRect;
+    [self.view addSubview:passwordField2];
+    
+    item_login = [[UIBarButtonItem alloc]initWithTitle:@"Log In" style:UIBarButtonItemStyleDone target:self action:@selector(loginCheck)];
+    item_logout = [[UIBarButtonItem alloc]initWithTitle:@"Log Out" style:UIBarButtonItemStyleDone target:self action:@selector(logoutCheck)];
+    item_register = [[UIBarButtonItem alloc]initWithTitle:@"Register" style:UIBarButtonItemStyleDone target:self action:@selector(registerUser)];
+    flexItem = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    
+    [self setToolbarItems:@[flexItem,item_register, flexItem] animated:YES];
     [self.navigationController setToolbarHidden:NO animated:YES];
 }
 
@@ -64,6 +102,10 @@
 
 -(void)logoutCheck{
     NSLog(@"logging out");
+}
+
+-(void)registerUser{
+    
 }
 
 -(void)closeKeyboard: (UITapGestureRecognizer*)tap{
@@ -83,21 +125,39 @@
     
     //register mode
     if([segControl selectedSegmentIndex] == 0){
-        
-    }
-    
-    //login mode
-    else if ([segControl selectedSegmentIndex] == 1){
         CGFloat indent = 10.f;
         
-        UITextField *nameField = [[UITextField alloc]init];
         nameField.frame = CGRectMake(indent, 250, width - indent*2, 50);
         nameField.placeholder = @"User Name";
         nameField.textAlignment = NSTextAlignmentCenter;
         nameField.borderStyle = UITextBorderStyleRoundedRect;
         [self.view addSubview:nameField];
         
-        UITextField *passwordField = [UITextField new];
+        passwordField.frame = CGRectMake(indent, 320, width-indent*2, 50);
+        passwordField.placeholder = @"Password";
+        passwordField.textAlignment = NSTextAlignmentCenter;
+        passwordField.borderStyle = UITextBorderStyleRoundedRect;
+        [self.view addSubview:passwordField];
+        
+        passwordField2.frame = CGRectMake(indent, 390, width-indent*2, 50);
+        passwordField2.placeholder = @"Enter Password Again";
+        passwordField2.textAlignment = NSTextAlignmentCenter;
+        passwordField2.borderStyle = UITextBorderStyleRoundedRect;
+        [self.view addSubview:passwordField2];
+        
+        [self setToolbarItems:@[flexItem, item_register, flexItem] animated:YES];
+    }
+    
+    //login mode
+    else if ([segControl selectedSegmentIndex] == 1){
+        CGFloat indent = 10.f;
+        
+        nameField.frame = CGRectMake(indent, 250, width - indent*2, 50);
+        nameField.placeholder = @"User Name";
+        nameField.textAlignment = NSTextAlignmentCenter;
+        nameField.borderStyle = UITextBorderStyleRoundedRect;
+        [self.view addSubview:nameField];
+        
         passwordField.frame = CGRectMake(indent, 320, width-indent*2, 50);
         passwordField.placeholder = @"Password";
         passwordField.textAlignment = NSTextAlignmentCenter;
@@ -123,6 +183,8 @@
         [rememberButton sizeToFit];
         [rememberButton addTarget:self action:@selector(rememberName) forControlEvents:UIControlEventTouchUpInside];
         [self.view addSubview:rememberButton];
+        
+        [self setToolbarItems:@[flexItem, item_login, flexItem, item_logout, flexItem] animated:YES];
     }
     
     else{
